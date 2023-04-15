7e0ee19c5a20e14dab8302f21177a6359aa984c0
Iblis1871 <chris.strafford@gmail.com>
2023-04-15 22:59:18 +0100

    articles can be edited by user in via link in show view, articles/new and articles/edit use _form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
   def index
-
     @articles = Article.all
   end
 
@@ -14,7 +13,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-  
+
     if @article.save
       redirect_to @article
     else
@@ -22,9 +21,22 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
     end
-
 end
